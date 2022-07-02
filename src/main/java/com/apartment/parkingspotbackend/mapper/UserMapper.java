5f12776957a44fbae0056e66f685c5f094a83850
yujung7768903 <yu-jung31476@naver.com>
2022-07-03 02:13:15 +0900

    feat: user crud 구현 완료

--- src/main/java/com/apartment/parkingspotbackend/mapper/UserMapper.java
+++ src/main/java/com/apartment/parkingspotbackend/mapper/UserMapper.java
@@ -8,4 +8,8 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
     public List<UserDto> getUserList();
+    public int insertUser(UserDto userDto);
+    public UserDto findByEmail(String email);
+    public int updateUser(UserDto userDto);
+    public int deleteUser(Integer id);
 }
--- src/main/java/com/apartment/parkingspotbackend/service/UserServiceImpelements.java
+++ src/main/java/com/apartment/parkingspotbackend/service/UserServiceImpelements.java
@@ -12,8 +12,57 @@ import java.util.List;
 public class UserServiceImpelements implements UserService{
     private final UserMapper userMapper;
 
+    // 모든 유저 조회
     @Override
     public List<UserDto> getUserList() {
         return userMapper.getUserList();
     }
+
+    // 이메일로 유저 찾기
+    @Override
+    public Integer findByEmail(String email) {
+        UserDto userDto = userMapper.findByEmail(email);
+        return userDto.getId();
+    }
+
+    // 유저 등록
+    @Override
+    public boolean insertUser(UserDto userDto) {
+        try {
+            userMapper.insertUser(userDto);
+            return true;
+        }catch (Exception e) {
+            System.out.println("error: " + e);
+            return false;
+        }
+    }
+
+    // 유저 정보 수정
+    @Override
+    public boolean updateUser(Integer id, UserDto userDto) {
+        System.out.println("전: " + userDto);
+        userDto.setId(id);
+        System.out.println("후: " + userDto);
+        try {
+            userMapper.updateUser(userDto);
+            return true;
+        }catch (Exception e) {
+            System.out.println("error: " + e);
+            return false;
+        }
+    }
+
+    // 유저 정보 삭제
+    @Override
+    public boolean deleteUser(Integer id) {
+        try {
+            userMapper.deleteUser(id);
+            return true;
+        }catch (Exception e) {
+            System.out.println("error: " + e);
+            return false;
+        }
+    }
+
+
 }
